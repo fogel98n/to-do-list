623e6bdbf6d452e15e584821f30a1d4ed1c63411
fogel <fogel@MacBook-Pro-fogel.local>
2025-07-29 07:46:54 -0600

    feat:header front-end terminado

--- components/header/header-component.js
+++ components/header/header-component.js
@@ -1,2 +1,16 @@
- export const header=document.createElement("header");
-header.className="header";
+export function Header() {
+  const header = document.createElement("header");
+  header.className = "header";
+
+  const titulo = document.createElement("h2");
+  titulo.className = "titulo";
+  titulo.textContent = "to-do-list";
+  header.appendChild(titulo);
+
+  const taresPendientes = document.createElement("p");
+  taresPendientes.className = "contenedor-tareas";
+  taresPendientes.textContent = "Tareas pendientes: 0";
+  header.appendChild(taresPendientes);
+
+  return header;
+}
--- vistas/deshboard-view.js
+++ vistas/deshboard-view.js
@@ -1,13 +1,11 @@
+import { Header } from "../components/header/header-component.js";
 export function deshboard(){
-    const deshboard=document.getElementById("deshboard")
     let section=document.createElement("section");
-    section.className="deshboard";
-
-    let h1=document.createElement("h1");
-    h1.textContent="Deshboard";
-    section.appendChild(h1);
-
-    return section
-
+    section.appendChild(Header());
+    return section;
 }
-document.body.appendChild(deshboard());
+
+const main = document.getElementById("deshboard");
+if (main) {
+    main.appendChild(deshboard());
+}
